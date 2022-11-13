d60ea3a451fcf1e979a971160a9ec88f27567373
Puneet Srivastava <srivastavap714@gmail.com>
2022-11-13 17:42:49 +0530

    Day4

--- hubspot/src/App.js
+++ hubspot/src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import HeaderTwo from './Components/HeaderTwo';
+import CardOne from './Components/CardOne';
+import CardTwo from './Components/CardTwo';
+import CardThree from './Components/CardThree';
+import CardFour from './Components/CardFour';
+
 
 function App() {
   return (
     <div className="app">
       <Header />
       <HeaderTwo />
+      <CardOne />
+      <CardTwo />
+      <CardThree />
+      <CardFour />
     </div>
   );
 }
